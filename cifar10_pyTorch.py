ff10473ab1f397caac09107667c9efda107cc2a4
voldemortX <xian_studio@hotmail.com>
2019-04-02 21:46:10 +0800

    PyTorch test network on CIFAR10

--- cifar10_pyTorch.py
+++ cifar10_pyTorch.py
@@ -1,1 +1,119 @@
 import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import time
+
+
+# Network architecture
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+# Draw images
+def show(images):
+    images = images / 2 + 0.5  # denormalize
+    np_images = images.numpy()
+    print(np_images.shape)
+    plt.imshow(np.transpose(np_images, (1, 2, 0)))
+    plt.show()
+
+
+# Show random images
+def visualize(loader):
+    temp = iter(loader)
+    images, labels = temp.next()
+    show(torchvision.utils.make_grid(images))
+    print(' '.join('%5s' % classes[labels[j]] for j in range(labels.size(0))))
+
+
+# Load data
+def init(batch_size):
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                              shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=transform)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                             shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    return train_loader, test_loader, classes
+
+
+# Train data
+def train(num_epochs):
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        time_now = time.time()
+        for i, data in enumerate(train_loader, 0):
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            if i % 100 == 99:
+                print('[%d, %d] loss: %.4f' % (epoch + 1, i + 1, running_loss / 100))
+                running_loss = 0.0
+
+        print('Epoch time: %.2fs' % (time.time() - time_now))
+
+
+# Test
+def inference(loader):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in loader:
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Test acc: %f' % (100 * correct / total))
+
+
+if __name__ == "__main__":
+    train_loader, test_loader, classes = init(32)
+    visualize(train_loader)
+    net = Net()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(device)
+    net.to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.005, momentum=0.9)
+    train(2)
+    inference(test_loader)
